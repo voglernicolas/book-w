111ce1099d440c8b99fc56e265e91aa63f7756b3
NicoV <nicolasvogler0@gmail.com>
2017-11-28 20:50:23 -0300

    Make async request to server and render server validation errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Message } from 'semantic-ui-react'
 import Validator from 'validator';
 import InLineError from '../messages/InLineError'
 import PropTypes from 'prop-types'
@@ -21,11 +21,14 @@ class LoginForm extends React.Component {
     const errors = this.validate(this.state.data)
     this.setState({ errors })
     if (Object.keys(errors).length === 0) {
-      this.props.submit(this.state.data)
+      this.setState({ loading: true })
+      this.props
+        .submit(this.state.data)
+        .catch(err => this.setState({ errors: err.response.data.errors, loading: false }))
     }
   }
 
-  validate = (data) => {
+  validate = data => {
     const errors = {}
     if (!Validator.isEmail(data.email)) errors.email = 'Invalid email'
     if (!data.password) errors.password = "Can't be empty"
@@ -33,10 +36,14 @@ class LoginForm extends React.Component {
   }
 
   render() {
-    const {data, errors} = this.state
+    const {data, errors, loading} = this.state
 
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        { errors.global && <Message negative>
+          <Message.Header>Something went wrong</Message.Header>
+          <p>{errors.global}</p>
+        </Message>}
         <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input type="text"
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
 
-export default combineReducers({
-  user: () => ({})
-})
+import user from './reducers/user'
+
+export default combineReducers({user})
